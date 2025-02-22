2a39cdcd9b288408402a875e14193acc8a91f676
dbratu777 <dab755@msstate.edu>
2025-02-22 20:04:02 +0000

    minor bugfixes

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 _SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$_SCRIPT_DIR" || exit 1
 
 if command -v conda &>/dev/null; then
     source "$(conda info --base)/etc/profile.d/conda.sh"
@@ -9,7 +10,10 @@ else
     exit 1
 fi
 
-conda activate fish-sensor-peripherals      || { echo "ERROR: failed to activate conda environment"; exit 1; }
+conda activate fish-sensor-peripherals                              || { echo "ERROR: failed to activate conda environment"; exit 1; }
 
 trap 'conda deactivate' EXIT
-python "$_SCRIPT_DIR"/sensor-peripherals.py || { echo "ERROR: failed to run fishapp.py"; exit 1; }
+cd "$_SCRIPT_DIR" || exit 1
+$(conda run -n fish-sensor-peripherals which python) sensor-peripherals.py   || { echo "ERROR: failed to run sensor-peripherals.py"; exit 1; }
+
+read -p "press ENTER to exit..."
